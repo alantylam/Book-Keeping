e5f2ea1b72718d41e6b0e461e1dca16aab8134df
alantylam <alanlam56@gmail.com>
2018-12-05 22:19:42 -0700

    Initial Commit

---
+++ Book Keeping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Book Keeping
+//
+//  Created by Alan Lam on 2018-12-05.
+//  Copyright © 2018 Alan Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Book KeepingTests/Book_KeepingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Book_KeepingTests.swift
+//  Book KeepingTests
+//
+//  Created by Alan Lam on 2018-12-05.
+//  Copyright © 2018 Alan Lam. All rights reserved.
+//
+
+import XCTest
+@testable import Book_Keeping
+
+class Book_KeepingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
